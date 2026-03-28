716342d0a02c4316984a833830dd9cf5ca3f468c
Pedras4 <199379974+Pedras4@users.noreply.github.com>
2026-03-28 16:48:43 +0000

    Add files via upload

---
+++ inteligencia artificial.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+rec = sr.Recognizer()
+
+with sr.Microphone() as mic:
+
+    rec.adjust_for_ambient_noise(mic)
+    print("Fale algo e aguarde ...")
+
+    audio = rec.listen(mic)
+
+    texto = rec.recognize_google(audio, language="pt-BR")
+    print(texto)
+
+    if "navegador" in texto:
+        os.system("start Chrome.exe")
+        
